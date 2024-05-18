60a33608003ba70e760d6fecc885e7a1961a0ccb
廷宇 蘇 <k34500864@outlook.com>
2024-05-18 16:48:05 +0800

    加入專案檔案。

---
+++ 7-12.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main(void) {
+    int a, i, num, sum = 0;
+    int n; // 用于存储位数
+
+    scanf("%d", &a);
+    n = (int)log10(a)+1; // 计算位数
+
+    for (i = 1; i <= n; i++) {
+        double temp = pow(10, i); // 计算10的i次方
+        //printf("%d", temp);
+        int digit = (int)temp; // 转换为整数
+        num = a % digit; // 使用整数类型的digit
+        (int)num = num / pow(10, i - 1);
+        sum += pow(num, n); // 注意这里是+=而不是=
+    }
+    if (sum == a)
+    {
+        printf("%d是Armstrong number", a);
+    }
+    return 0;
+}
