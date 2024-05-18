3fc180e7c3f2a1ccbbd5b1d78017be2dce2ec1d6
廷宇 蘇 <k34500864@outlook.com>
2024-05-18 21:26:47 +0800

    #找出了除了0的Armstrong number

--- 7-12.c
+++ 7-12.c
@@ -2,23 +2,29 @@
 #include<math.h>
 
 int main(void) {
-    int a, i, num, sum = 0;
-    int n; // 用于存储位数
+    unsigned long long a = 0;
+    int n, b = 1, i, num, sum = 0; // 用于存储位数
 
-    scanf("%d", &a);
-    n = (int)log10(a)+1; // 计算位数
+    //scanf("%d", &a);
 
-    for (i = 1; i <= n; i++) {
-        double temp = pow(10, i); // 计算10的i次方
-        //printf("%d", temp);
-        int digit = (int)temp; // 转换为整数
-        num = a % digit; // 使用整数类型的digit
-        (int)num = num / pow(10, i - 1);
-        sum += pow(num, n); // 注意这里是+=而不是=
-    }
-    if (sum == a)
+    do
     {
-        printf("%d是Armstrong number", a);
-    }
+        a += 1;
+        sum = 0;
+        n = (int)log10(a)+1; // 计算位数
+        for (i = 1; i <= n; i++) {
+            double temp = pow(10, i); // 计算10的i次方
+            //printf("%d", temp);
+            int digit = (int)temp; // 转换为整数
+            num = a % digit; // 使用整数类型的digit
+            (int)num = num / pow(10, i - 1);
+            sum += pow(num, n); // 注意这里是+=而不是=
+        }
+        if (sum == a)
+        {
+            printf("%d.%d是Armstrong number\n", b, a);
+            b -= -1;
+        }
+    } while (a < 18446744073709551615);
     return 0;
 }
